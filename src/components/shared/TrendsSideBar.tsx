1fbab1b64c9db851f2c5bc3f82bb08ae6eb75651
sourav bhowal <souravbhowal15@gmail.com>
2024-07-30 21:10:27 +0530

    right side bar

--- src/components/shared/TrendsSideBar.tsx
+++ src/components/shared/TrendsSideBar.tsx
@@ -6,20 +6,23 @@ import Link from "next/link";
 import { Suspense } from "react";
 import UserAvatar from "./UserAvatar";
 import { Button } from "../ui/button";
+import { unstable_cache } from "next/cache";
+import { formatNumber } from "@/utils/topicsCount";
 
 // TRENDS BAR
 export default function TrendsSideBar() {
   return (
     <section className="sticky top-[5.25rem] hidden h-fit w-72 flex-none space-y-5 md:block lg:w-80">
-        {/* Suspence helps to load the component itself without blocking the ui */}
+      {/* Suspence helps to load the component itself without blocking the ui */}
       <Suspense fallback={<Loader2 className="mx-auto animate-spin" />}>
-        <WhoToFollow /> 
+        <WhoToFollow />
+        <TrendingTopics />
       </Suspense>
     </section>
   );
 }
 
-// WHO TO FOLLOW SERVER FUNCTION
+// WHO TO FOLLOW SERVER FUNCTION AND COMPONENT
 async function WhoToFollow() {
   // TAKE USER FROM SESSION
   const { user } = await validateRequest();
@@ -38,20 +41,71 @@ async function WhoToFollow() {
   return (
     <div className="space-y-5 rounded-2xl bg-card p-5 shadow-md">
       <div className="text-xl font-bold">Who to follow</div>
-      {
-        usersToFollow.map((user) => (
-            <div key={user.id} className="flex items-center justify-between gap-3">
-                <Link href={`/users/${user.username}`} className="flex items-center gap-3">
-                    <UserAvatar avatarUrl={user.avatarUrl} className="flex-none"/>
-                    <div>
-                        <p className="line-clamp-1 break-all font-semibold hover:underline">{user.name}</p>
-                        <p className="line-clamp-1 break-all text-muted-foreground">@{user.username}</p>
-                    </div>
-                </Link>
-                <Button>Follow</Button>
+      {usersToFollow.map((user) => (
+        <div key={user.id} className="flex items-center justify-between gap-3">
+          <Link
+            href={`/users/${user.username}`}
+            className="flex items-center gap-3"
+          >
+            <UserAvatar avatarUrl={user.avatarUrl} className="flex-none" />
+            <div>
+              <p className="line-clamp-1 break-all font-semibold hover:underline">
+                {user.name}
+              </p>
+              <p className="line-clamp-1 break-all text-muted-foreground">
+                @{user.username}
+              </p>
             </div>
-        ))
-      }
+          </Link>
+          <Button>Follow</Button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+// CACHING FUNCTION FOR DB CALL
+const getTrendingTopics = unstable_cache(
+  async () => {
+    const topics = await prisma.$queryRaw<{ hastag: string; count: bigint }[]>`
+    SELECT LOWER(unnest(regexp_matches(content, '#[[:alnum:]_]+', 'g'))) AS hastag, COUNT(*) AS count FROM posts GROUP BY (hastag) ORDER BY count DESC, hastag ASC LIMIT 5
+    `;
+
+    return topics.map((row) => ({
+      hastag: row.hastag,
+      count: Number(row.count),
+    }));
+  },
+  ["trending_topics"],
+  {
+    revalidate: 3 * 60 * 60,
+  },
+);
+
+// TRENDING TOPICS SERVER FUNCTION & COMPONENT
+async function TrendingTopics() {
+  const trendingTopics = await getTrendingTopics();
+
+  return (
+    <div className="space-y-5 rounded-2xl bg-card p-5 shadow-sm">
+      <div className="text-xl font-bold">Trending topics</div>
+      {trendingTopics.map(({ hastag, count }) => {
+        const title = hastag.split("#")[1];
+
+        return (
+          <Link key={title} href={`/hastag/${title}`} className="block">
+            <p
+              className="line-clamp-1 break-all font-semibold hover:underline"
+              title={hastag}
+            >
+              {hastag}
+            </p>
+            <p className="text-sm text-muted-foreground">
+              {formatNumber(count)} {count === 1 ? "post" : "posts"}
+            </p>
+          </Link>
+        );
+      })}
     </div>
   );
 }
