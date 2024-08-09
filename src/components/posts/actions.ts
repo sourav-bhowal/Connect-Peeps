e4728a521816887f60cb90a422a42db702b0ba32
Sourav Bhowal <souravbhowal15@gmail.com>
2024-08-10 01:55:00 +0530

    delete post pending

---
+++ src/components/posts/actions.ts
@@ -0,0 +1,31 @@
+"use server";
+import { prisma } from "@/lib/prismaDB";
+import { validateRequest } from "@/utils/auth";
+import { PostCardDataType } from "@/utils/types";
+
+export async function deletePost(id: string) {
+    // take user from session
+  const { user } = await validateRequest();
+
+  // if no user
+  if (!user) throw new Error("Unauthorized.");
+
+  // find post
+  const post = await prisma.post.findUnique({
+    where: { id },
+  });
+
+  // if no post
+  if (!post) throw new Error("Post not found.");
+
+  // if post user id and current user id not match
+  if (post.userId !== user.id) throw new Error("unauthorized");
+
+  // delete post
+  const deletedPost = await prisma.post.delete({
+    where: { id },
+    include: PostCardDataType
+  });
+
+  return deletedPost;
+}
---
+++ src/components/posts/mutations.ts
@@ -0,0 +1,63 @@
+import { PostCardData, PostsPage } from "@/utils/types";
+import { useToast } from "../ui/use-toast";
+import {
+  InfiniteData,
+  QueryFilters,
+  useMutation,
+  useQueryClient,
+} from "@tanstack/react-query";
+import { usePathname, useRouter } from "next/navigation";
+import { deletePost } from "./actions";
+
+export function useDeletePostMutation() {
+  // TOAST
+  const { toast } = useToast();
+  // QUERY CLIENT
+  const queryClient = useQueryClient();
+  // ROUTER
+  const router = useRouter();
+  // PATHNAME
+  const pathName = usePathname();
+
+  // Mutation
+  const mutation = useMutation({
+    mutationFn: deletePost,
+    onSuccess: async (deletedPost) => {
+      const queryFilter: QueryFilters = { queryKey: ["post-feed"] };
+
+      await queryClient.cancelQueries(queryFilter);
+
+      queryClient.setQueriesData<InfiniteData<PostsPage, string | null>>(
+        queryFilter,
+        (oldData) => {
+          if (!oldData) return;
+
+          return {
+            pageParams: oldData.pageParams,
+            pages: oldData.pages.map((page) => ({
+              nextCursor: page.nextCursor,
+              posts: page.posts.filter((post) => post.id !== deletedPost.id),
+            })),
+          };
+        },
+      );
+
+      toast({
+        description: "Post deleted.",
+      });
+
+      if (pathName === `/posts/${deletedPost.id}`) {
+        router.push("/");
+      }
+    },
+    onError(error) {
+      console.log(error);
+      toast({
+        variant: "destructive",
+        description: "Failed to delete post. Try again",
+      });
+    },
+  });
+
+  return mutation;
+}
