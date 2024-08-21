30c16767894d2917ae0ba2ddbb87c7da1ec52d0e
Sourav Bhowal <souravbhowal15@gmail.com>
2024-08-22 01:21:40 +0530

    done

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,5 +1,5 @@
-import { Loader2 } from "lucide-react";
+import { Loader } from "@/components/shared/Loader";
 
 export default function Loading() {
-    return <Loader2 className="mx-auto my-3 animate-spin" />
+  return <Loader />;
 }
